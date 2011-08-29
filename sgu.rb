2941a2173afcb038cf608de11592fb6d26280673
Sean Marcia <smarcia@gmail.com>
2011-08-29 00:30:18 -0400

    Added check to see if file already existed.

--- sgu.rb
+++ sgu.rb
@@ -6,12 +6,16 @@ require 'open-uri'
 url = 'http://www.theskepticsguide.org/archive/podcast.aspx?mid=1'
 doc = Nokogiri::HTML(open(url))
 
-mp3_links = doc.xpath("//a").select {|link| link['href'] =~ /\.mp3$/ }
- mp3_links.each do |link|
-   href = link['href']
-   outname = File.basename(href)
-   puts "Downloading: #{outname}"
-   open(href) do |io|
-     File.open(outname,'w') {|out| out.print(io.read) }
+sgu_mp3s = doc.xpath("//a").select {|link| link['href'] =~ /\.mp3$/ }
+ sgu_mp3s.each do |link|
+   xpathhref = link['href']
+   sgu_episode = File.basename(xpathhref)
+   if File.exists?(sgu_episode)
+     puts "Skipping download of #{sgu_episode}, it already exists!"
+   else
+     puts "Downloading: #{sgu_episode}"
+     open(xpathhref) do |save|
+       File.open(sgu_episode,'w') {|out| out.print(save.read) }
+     end
    end
- end
+end
