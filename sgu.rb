c5cc6b6a3c45d920edcb543b825da76d2238300c
Sean Marcia <smarcia@gmail.com>
2011-08-29 02:22:18 -0400

    Formatting fix

--- sgu.rb
+++ sgu.rb
@@ -5,15 +5,16 @@ url = 'http://www.theskepticsguide.org/archive/podcast.aspx?mid=1'
 doc = Nokogiri::HTML(open(url))
 
 sgu_mp3s = doc.xpath("//a").select {|link| link['href'] =~ /\.mp3$/ }
- sgu_mp3s.each do |link|
-   xpathhref = link['href']
-   sgu_episode = File.basename(xpathhref)
-   if File.exists?(sgu_episode)
-     puts "Skipping download of #{sgu_episode}, it already exists!"
-   else
-     puts "Downloading: #{sgu_episode}"
-     open(xpathhref) do |save|
-       File.open(sgu_episode,'w') {|out| out.print(save.read) }
-     end
-   end
+
+sgu_mp3s.each do |link|
+  xpathhref = link['href']
+  sgu_episode = File.basename(xpathhref)
+  if File.exists?(sgu_episode)
+    puts "Skipping download of #{sgu_episode}, it already exists!"
+  else
+    puts "Downloading: #{sgu_episode}"
+    open(xpathhref) do |save|
+      File.open(sgu_episode,'w') {|out| out.print(save.read) }
+    end
+  end
 end
